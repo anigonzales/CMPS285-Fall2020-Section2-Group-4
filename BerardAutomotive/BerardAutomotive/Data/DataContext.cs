ee0d6b19d5c4eecc0b15295b6cf047751bf1b3a8
Dominique Roubique <dominique.roubique@selu.edu>
2020-09-07 18:42:17 -0500

    Project Init

---
+++ BerardAutomotive/BerardAutomotive/Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using BerardAutomotive.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BerardAutomotive.Features.Service;
+
+namespace BerardAutomotive.Data
+{
+    public class DataContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public DataContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        }
+
+        public DbSet<BerardAutomotive.Features.Service.Service> Service { get; set; }
+    }
+}
---
+++ BerardAutomotive/BerardAutomotive/Features/Service/Service.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BerardAutomotive.Features.Service
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Eta { get; set; }
+
+    }
+
+    public class ServiceConfiguration : IEntityTypeConfiguration<Service>
+    {
+        public void Configure(EntityTypeBuilder<Service> builder)
+        {
+           
+        }
+    }
+}
