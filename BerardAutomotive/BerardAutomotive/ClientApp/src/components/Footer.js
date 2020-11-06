48985e4cb3534e22363f72751c23b2ff69db8ba2
Devon Clark <devonclark36@gmail.com>
2020-11-06 13:17:24 -0600

    KEVINNNNN

--- BerardAutomotive/BerardAutomotive/ClientApp/src/components/Footer.js
+++ BerardAutomotive/BerardAutomotive/ClientApp/src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../Footer.css';
+import LoginPopup from './Login-popup'
 
 function Footer() {
     return (
         <div className="Footer">
             <footer>
-                <p>Berard's Automotive Login Area</p>
+                <LoginPopup />
             </footer>
         </div>
     )
